3b8ead81bf1652602922cce25b722026ce302a52
Simone Basso <bassosimone@gmail.com>
2025-12-30 05:46:44 +0100

    feat(flagset_test.go): add more tests

---
+++ flagset_test.go
@@ -0,0 +1,37 @@
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// Adapted from: https://github.com/bassosimone/clip/blob/v0.8.0/pkg/nflag/flagset_test.go
+//
+
+package vflag
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlagSetSetInvalidValue(t *testing.T) {
+	fset := NewFlagSet("test", ContinueOnError)
+	var value int64
+	fset.Int64Var(&value, 0, "count")
+	err := fset.Parse([]string{"--count", "not-a-valid-number"})
+	assert.Error(t, err)
+}
+
+func TestFlagSetMaybeHandleError(t *testing.T) {
+	t.Run("PanicOnError", func(t *testing.T) {
+		fset := NewFlagSet("test", PanicOnError)
+		assert.Panics(t, func() {
+			fset.Parse([]string{"--unknown"})
+		})
+	})
+
+	t.Run("ContinueOnError", func(t *testing.T) {
+		fset := NewFlagSet("test", ContinueOnError)
+		fset.AutoHelp('h', "help", "Show this help message and exit.")
+		err := fset.Parse([]string{"--help"})
+		assert.ErrorIs(t, err, ErrHelp)
+	})
+}
